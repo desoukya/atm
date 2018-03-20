1869193e434cf4d5c4113c20046dce899f4d9c1e
johnstout <john.j.stout@aol.com>
2018-03-20 12:09:58 -0400

    removed knex

--- src/server/seeds/01_atm_users.js
+++
@@ -1,8 +0,0 @@
-const users = require('../seed_data/users_seed');
-
-exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
-      return knex('users').insert(users);
-    });
-};
