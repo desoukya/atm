c11dd88af7fc94b8d3568873b7976de1c701da33
johnstout <john.j.stout@aol.com>
2018-03-20 12:24:14 -0400

    initial

--- src/server/seeds/02_atm_accounts.js
+++
@@ -1,8 +0,0 @@
-const accounts = require('../seed_data/accounts_seed');
-
-exports.seed = function(knex, Promise) {
-  return knex('accounts').del()
-    .then(function () {
-      return knex('accounts').insert(accounts);
-    });
-};
